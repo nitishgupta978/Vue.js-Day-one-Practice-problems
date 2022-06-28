cd6e75353e272046286b60885ad66b986ce8c59f
nitishgupta978 <nitishgupta9785@gmail.com>
2022-06-28 16:16:37 +0530

    Added:Obligatory Example Hello World!

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+ <div id="app">
+  {{ message }} Nice to meet Vue.
+</div>
+</template>
+
+<script>
+export default {
+   name: 'HelloWorld',
+   
+  data() {
+   return {
+        message: 'Hello world!'
+    }
+  }
+}
+</script>
